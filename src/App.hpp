bf319879d86fb1f7d8d6981f3d34e0ee808a2328
pvinnbru <piet.vinnbruck@gmail.com>
2025-04-20 14:44:51 +0000

    blatt0

--- src/App.hpp
+++ src/App.hpp
@@ -18,23 +18,66 @@
 #include "Image.hpp"
 #include "ObjectLoader.hpp"
 #include "Resources.hpp"
+#include "Shader.hpp"
 
 class App {
 public:
-    App() { }
+    App() { 
+
+        // simple triangle
+        triangleVertexBufferData = {
+            -1.0f, -1.0f, 0.0f,
+             1.0f, -1.0f, 0.0f,
+             0.0f,  1.0f, 0.0f,
+        };
+
+        // haus vom nikolaus
+        houseVertexBufferData = {
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+            -0.5f,  0.5f, 0.0f,
+             0.5f,  0.5f, 0.0f,
+             0.0f,  1.0f, 0.0f,
+            -0.5f,  0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+             0.5f,  0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f
+        };
+
+        g_vertex_buffer_data = triangleVertexBufferData;
+
+    }
+
+    void updateVertexBuffer() {
+        glGenBuffers(1, &vertexbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBufferData(GL_ARRAY_BUFFER, g_vertex_buffer_data.size() * sizeof(GLfloat), g_vertex_buffer_data.data(), GL_STATIC_DRAW);
+    }
+
+    void initializeVAO() {
+        GLuint vao;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+    }
 
     void init(GLFWwindow* window)
     {
-        (void)window;
+        initializeVAO();
+        updateVertexBuffer();
+        programID = Shader::loadShaders(to_resource_path("vertex0.glsl"), to_resource_path("fragment0.glsl"));
+        glUseProgram(programID);
     }
 
     void draw(GLFWwindow* window, float delta_time)
     {
-        (void)window;
-        (void)delta_time;
-
-        glClearColor(1.0f, 0.2f, 0.2f, 1.0f);
+        glClearColor(1.0f, 1.0f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(drawingMode, 0, verticesCount);
+        glDisableVertexAttribArray(0);
     }
 
     void on_key_change(GLFWwindow* window, int key, int scancode, int action, int mode)
@@ -45,6 +88,20 @@ public:
         if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, GL_TRUE);
         }
+
+        if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
+            g_vertex_buffer_data = triangleVertexBufferData;
+            drawingMode = GL_TRIANGLES;
+            verticesCount = 3;
+            updateVertexBuffer();
+        }
+
+        if (key == GLFW_KEY_2 && action == GLFW_PRESS) {
+            g_vertex_buffer_data = houseVertexBufferData;
+            drawingMode = GL_LINE_STRIP;
+            verticesCount = 9;
+            updateVertexBuffer();
+        }
     }
 
     void on_resize(GLFWwindow* window, int width, int height)
@@ -54,4 +111,13 @@ public:
     }
 
 private:
+
+    std::vector<GLfloat> g_vertex_buffer_data;
+    std::vector<GLfloat> triangleVertexBufferData;
+    std::vector<GLfloat> houseVertexBufferData;
+
+    GLuint vertexbuffer;
+    GLuint programID;
+    GLenum drawingMode = GL_TRIANGLES;
+    int verticesCount = 3;
 };
